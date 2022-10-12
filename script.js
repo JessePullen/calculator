d7e037b75b5f617083deeba9426df4ff92e17001
JessePullen <jessepullen@icloud.com>
2022-10-12 16:19:38 +1000

    Add button event listeners

--- script.js
+++ script.js
@@ -21,6 +21,55 @@ const display = document.querySelector('.display');
 const totalDisplay = document.querySelector('.total-display');
 
 
+let total = [];
+display.textContent = null;
+totalDisplay.textContent = total;
+
+
+//Adds event listeners to buttons on the calculator
+zero.addEventListener('click', () => {
+    display.textContent += 0;
+});
+one.addEventListener('click', () => {
+    display.textContent += 1;
+});
+two.addEventListener('click', () => {
+    display.textContent += 2;
+});
+three.addEventListener('click', () => {
+    display.textContent += 3;
+});
+four.addEventListener('click', () => {
+    display.textContent += 4;
+});
+five.addEventListener('click', () => {
+    display.textContent += 5;
+});
+six.addEventListener('click', () => {
+    display.textContent += 6;
+});
+seven.addEventListener('click', () => {
+    display.textContent += 7;
+});
+eight.addEventListener('click', () => {
+    display.textContent += 8;
+});
+nine.addEventListener('click', () => {
+    display.textContent += 9;
+});
+period.addEventListener('click', () => {
+    display.textContent += '.';
+});
+
+//Function buttons
+clear.addEventListener('click', () => {
+    display.textContent = null;
+    totalDisplay.textContent = null;
+    total = [];
+});
+ac.addEventListener('click', () => {
+    display.textContent = null;
+});
 
 function addition(num1, num2) {
     return num1 + num2;
@@ -40,28 +89,21 @@ function division(num1, num2) {
 
 
 //Takes an operator and 2 numbers and then calls the specified function to calculate the result
-function operate() {
-    let operator = prompt('+ - * /');
-    let num1 = parseInt(prompt('Number 1'));
-    let num2 = parseInt(prompt ('Number 2'));
+function operate(operator, num1, num2) {
+    // let operator = prompt('+ - * /');
     let result = 0;
 
-    if (operator === '+') {
+    if (operator === ' + ') {
         operatorType = addition;
-    } else if (operator === '-') {
-        operatorType = ion;
-    } else if (operator === '*') {
+    } else if (operator === ' - ') {
+        operatorType = subtraction;
+    } else if (operator === ' * ') {
         operatorType = multiplication;
-    } else if (operator === '/') {
-        operatorType =sion;
+    } else if (operator === ' / ') {
+        operatorType = division;
     }
 
     result = operatorType(num1, num2);
 
-    return result;
+    display.textContent = result;
 }
-
-function runProgram() {
-}
-
-runProgram();
